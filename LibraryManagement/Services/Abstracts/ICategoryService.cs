125c6b114a9fef193609f0d99b01d3e6d28812f7
BerkaySimsek <berkayysimsekk@gmail.com>
2025-03-11 01:10:06 +0300

    CategoryService Added

---
+++ LibraryManagement/Services/Abstracts/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using LibraryManagement.Models.Dtos.Categories;
+using System.Xml.Serialization;
+
+namespace LibraryManagement.Services.Abstracts;
+
+public interface ICategoryService
+{
+    List<CategoryResponseDto> GetAll();
+    void Add(CategoryAddRequestDto categoryAddRequestDto);
+    void Delete(int id);
+    CategoryResponseDto GetById(int id);
+}
---
+++ LibraryManagement/Services/Concretes/CategoryService.cs
@@ -0,0 +1,62 @@
+﻿using LibraryManagement.DataAccess.Abstracts;
+using LibraryManagement.DataAccess.Concretes;
+using LibraryManagement.Models;
+using LibraryManagement.Models.Dtos.Categories;
+using LibraryManagement.Services.Abstracts;
+
+namespace LibraryManagement.Services.Concretes;
+
+public class CategoryService : ICategoryService
+{
+    ICategoryRepository categoryRepository = new CategoryRepository();
+    public void Add(CategoryAddRequestDto categoryAddRequestDto)
+    {
+        Category category = ConvertToCategory(categoryAddRequestDto);
+        categoryRepository.Add(category);
+    }
+
+    public void Delete(int id)
+    {
+        Category category = categoryRepository.GetById(id);
+        categoryRepository.Delete(category);
+    }
+
+    public List<CategoryResponseDto> GetAll()
+    {
+        List<Category> categories = categoryRepository.GetAll();
+        List<CategoryResponseDto> responses = ConvertToResponseDtoList(categories);
+        return responses;
+    }
+
+    public CategoryResponseDto GetById(int id)
+    {
+        Category category = categoryRepository.GetById(id);
+        CategoryResponseDto? response = ConvertToResponseDto(category);
+        return response;
+    }
+    private Category ConvertToCategory(CategoryAddRequestDto dto)
+    {
+        return new Category { Name = dto.Name };
+    }
+    private CategoryResponseDto ConvertToResponseDto(Category category)
+    {
+        return new CategoryResponseDto
+        {
+            Id = category.Id,
+            Name = category.Name,
+        };
+    }
+    private List<CategoryResponseDto?> ConvertToResponseDtoList(List<Category> categories)
+    {
+        // 1. Yöntem
+        //List<CategoryResponseDto> responses = new();
+        //foreach (Category category in categories)
+        //{
+        //    CategoryResponseDto response=ConvertToResponseDto(category);
+        //    responses.Add(response);
+        //}
+
+        // 2. Yöntem
+        return categories.Select(x => ConvertToResponseDto(x)).ToList();
+    }
+}
